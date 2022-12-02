669a88738e7d4c2477614f6ca65aca10487fe9df
Gustavo <gpontesf06@gmail.com>
2022-12-02 14:40:52 -0300

    Gerenciador de telas adicionado

--- Options/Program.cs
+++ Options/Program.cs
@@ -5,30 +5,7 @@
         static void Main(string[] args)
         {
             Options opcoes = new Options();
-            opcoes.AddOpcao(new Option("Teste", new Screen(
-                () => {
-                    Console.Clear();
-                    Console.WriteLine("oi");
-                },
-                () =>
-                {
-                    opcoes.AddOpcao(new Option("oii", new Screen(
-                        () => {
-                            Console.Clear();
-                            Console.WriteLine("oiia");
-                            Console.ReadKey();
-                        }, () => { })));
-                    opcoes.AddOpcao(new Option("oii", new Screen(
-                        () => {
-                            Console.Clear();
-                            Console.WriteLine("ola");
-                            Console.ReadKey();
-                        }, () => { })));
-                }
-                )));
-
-            // opcoes.AddOpcao(new Option("Teste2"));
-            //opcoes.AddOpcao(new Option("Teste3"));
+            opcoes.AddOpcao(new Option("Teste", ScreenManager.Screen1(opcoes)));
 
             opcoes.EscreveOpcoes();
 
---
+++ Options/ScreenManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Options
+{
+    public class ScreenManager
+    {
+        public static Screen Screen1(Options opcoes)
+        {
+            return new Screen(() =>
+            {
+                Console.Clear();
+                Console.WriteLine("oi");
+            },
+                () =>
+                {
+                    opcoes.AddOpcao(new Option("Cumprimentar", Cumprimentar()));
+                    opcoes.AddOpcao(new Option("Despedir", Despedir()));
+                }
+                );
+        }
+
+        public static Screen Cumprimentar()
+        {
+            return new Screen(
+                        () =>
+                        {
+                            Console.Clear();
+                            Console.WriteLine("oiia");
+                            Console.ReadKey();
+                        }, () => { });
+        }
+        public static Screen Despedir()
+        {
+            return new Screen(
+                        () =>
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Tchau");
+                            Console.ReadKey();
+                        }, () => { });
+        }
+    }
+}
